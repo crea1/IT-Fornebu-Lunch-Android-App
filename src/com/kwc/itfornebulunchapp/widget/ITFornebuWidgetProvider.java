cb8bc24fe062aafa101113c8fcc73b9ec703949c
Marius Kristensen <marius.kristensen@gmail.com>
2012-12-22 17:01:27 +0100

    Update widget text via widget provider.

--- src/com/kwc/itfornebulunchapp/widget/ITFornebuWidgetProvider.java
+++ src/com/kwc/itfornebulunchapp/widget/ITFornebuWidgetProvider.java
@@ -2,7 +2,13 @@ package com.kwc.itfornebulunchapp.widget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.util.Log;
+import android.widget.RemoteViews;
+import com.kwc.itfornebulunchapp.R;
+
+import java.util.Random;
 
 /**
  * @author Marius Kristensen
@@ -10,8 +16,24 @@ import android.content.Context;
  */
 public class ITFornebuWidgetProvider extends AppWidgetProvider {
 
+    private static final String LOGTAG = "LUNCHWIDGET";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);    //To change body of overridden methods use File | Settings | File Templates.
+        ComponentName lunchWidget = new ComponentName(context, ITFornebuWidgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(lunchWidget);
+
+        for (int widgetId : allWidgetIds) {
+            String updatedText = "someText :D " +new Random().nextInt(100);
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+            Log.w(LOGTAG, "updating widet with text: " + updatedText);
+            //Update widget text
+            remoteViews.setTextViewText(R.id.update, updatedText);
+
+            // Update the widget
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
     }
 }
