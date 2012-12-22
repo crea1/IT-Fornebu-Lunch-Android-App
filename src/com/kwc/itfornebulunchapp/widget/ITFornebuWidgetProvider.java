15370f000f00fd5a7759c560c0b8d40444543521
Marius Kristensen <marius.kristensen@gmail.com>
2012-12-22 16:31:01 +0100

    Added empty widget

---
+++ src/com/kwc/itfornebulunchapp/widget/ITFornebuWidgetProvider.java
@@ -0,0 +1,17 @@
+package com.kwc.itfornebulunchapp.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * @author Marius Kristensen
+ * @since 1.3
+ */
+public class ITFornebuWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
