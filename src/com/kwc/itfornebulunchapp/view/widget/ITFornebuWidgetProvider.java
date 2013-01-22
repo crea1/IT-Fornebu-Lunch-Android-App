cb7e83eb2bb5b45a6448f4930981ad1dc5d4ae76
Marius Kristensen <marius.kristensen@gmail.com>
2013-01-22 20:09:06 +0100

    Small widget updates data onclick

--- src/com/kwc/itfornebulunchapp/view/widget/ITFornebuWidgetProvider.java
+++ src/com/kwc/itfornebulunchapp/view/widget/ITFornebuWidgetProvider.java
@@ -29,11 +29,14 @@ public class ITFornebuWidgetProvider extends AppWidgetProvider {
         int[] allWidgetIds = appWidgetManager.getAppWidgetIds(lunchWidget);
 
         for (int widgetId : allWidgetIds) {
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            remoteViews.setTextViewText(R.id.update, "Oppdaterer...");
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
             WeekMenuService weekMenuService = new WeekMenuService();
             DayMenu todaysMenu = weekMenuService.getTodaysDish();
             String updatedDish = todaysMenu.getDish();
 
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 
             Log.d(LOGTAG, "updating widget(2x1) with text: " + updatedDish);
             //Update widget text
@@ -51,11 +54,13 @@ public class ITFornebuWidgetProvider extends AppWidgetProvider {
     private void setupOnClickListener(Context context, int[] appWidgetIds, RemoteViews remoteViews) {
 
         // The onclick should load the main activity
-        Intent intent = new Intent(context, LunsjActivity.class);
+        //Intent intent = new Intent(context, LunsjActivity.class);
+        Intent intent = new Intent(context, ITFornebuWidgetProvider.class);
         intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        //PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         remoteViews.setOnClickPendingIntent(R.id.smallWidgetLayout, pendingIntent);
     }
